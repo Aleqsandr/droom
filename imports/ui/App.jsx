102cd48e2dfca0c875cdebd73aff5527b8d1f0b1
Tencap <noisiv@outlook.fr>
2017-01-09 13:21:11 +0100

    kanva added

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
-import ReactCanvas from 'react-canvas';
+import {Layer, Rect, Stage, Group} from 'react-konva';
 
-var Surface = ReactCanvas.Surface;
-var Layer = ReactCanvas.Sayer;
-var Image = ReactCanvas.Image;
-var Text = ReactCanvas.Text;
+// Simple drawing rectangle class
+class MyRect extends React.Component {
+    constructor(...args) {
+      super(...args);
+      this.state = {
+        color: 'green'
+      };
+      this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+      this.setState({
+        color: Konva.Util.getRandomColor()
+      });
+    }
+    render() {
+        return (
+            <Rect
+                x={10} y={10} width={50} height={50}
+                fill={this.state.color}
+                shadowBlur={10}
+                onClick={this.handleClick}
+            />
+        );
+    }
+}
 
 // App component - represents the whole app
 export default class App extends Component {
 
   render() {
-
-
     return (
       <div className="container">
         <header>
           <h1>Drumloop</h1>
         </header>
         <main>
-          <Surface class="game-window" width='800' height='600'>
-
-          </Surface>
+          <Stage width={700} height={700}>
+            <Layer>
+                <MyRect/>
+            </Layer>
+          </Stage>
         </main>
       </div>
     );
