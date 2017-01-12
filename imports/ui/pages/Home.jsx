059535639ed2a76d1cd39e87939af9db3620be00
Alexandre Reho <alexandre.reho@gmail.com>
2017-01-12 13:34:28 +0100

    home page fading + rooting

--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -8,14 +8,16 @@ export default class Home extends Component {
       <div className="Home-container">
         <nav className="nav">
           <ul className="nav__list">
-            <li className="list__item">Register</li>
-            <li className="list__item">Login</li>
-            <li className="list__item">About</li>
+            <li className="nav__list__item"><Link to="/app">LET ME PLAY</Link></li>
+            <li className="nav__list__item">ABOUT</li>
           </ul>
         </nav>
+        <div className="bg1"></div>
+        <div className="bg2"></div>
+        <div className="bg3"></div>
+        <div className="bg4"></div>
         <div className="home">
           <img className="home__logo" src="/img/mainlogo.gif"></img>
-          <button className="home__playbtn"><Link to="/app">PLAY</Link></button>
         </div>
       </div>
     );
