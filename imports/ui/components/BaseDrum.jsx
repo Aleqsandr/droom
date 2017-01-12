8cd96b42d8a37ad4603089b63582a991194e7193
Tencap <noisiv@outlook.fr>
2017-01-12 11:01:49 +0100

    ui modified

--- imports/ui/components/BaseDrum.jsx
+++ imports/ui/components/BaseDrum.jsx
@@ -12,11 +12,15 @@ export default class BaseDrum extends Component {
     let size = 50, padding = 25, y = window.innerHeight - size - padding;
     return (
       <Layer>
-        <Group ref="groupReceiver" y={window.innerHeight - size - padding} x={window.innerWidth/2}>
-          <BaseReceiver x={0} size={size} color="#ff0000" note={38}/>
-          <BaseReceiver x={size*1+padding} size={size} color="#00ff00" note={40}/>
-          <BaseReceiver x={size*2+padding*2} size={size} color="#fff000" note={44}/>
-          <BaseReceiver x={size*3+padding*3} size={size} color="#ff00ff" note={36}/>
+        <Group ref="groupReceiver" y={window.innerHeight - size - padding} x={window.innerWidth / 3}>
+          <BaseReceiver x={0} size={size} color="#BDBDBD" note={46}/>
+          <BaseReceiver x={size*1+padding} size={size} color="#9CCC65" note={49}/>
+          <BaseReceiver x={size*2+padding*2} size={size} color="#FFCA28" note={38}/>
+          <BaseReceiver x={size*3+padding*3} size={size} color="#FF7043" note={48}/>
+          <BaseReceiver x={size*4+padding*4} size={size} color="#26A69A" note={36}/>
+          <BaseReceiver x={size*5+padding*5} size={size} color="#5C6BC0" note={45}/>
+          <BaseReceiver x={size*6+padding*6} size={size} color="#ef5350" note={43}/>
+          <BaseReceiver x={size*7+padding*7} size={size} color="#AB47BC" note={51}/>
         </Group>
       </Layer>
     );
