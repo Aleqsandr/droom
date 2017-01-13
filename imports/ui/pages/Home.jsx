a0f7f7fec8a981a4b2b23e2ca8d6a0f25ca67818
Matthieu <matthieubessol@gmail.com>
2017-01-13 16:40:13 +0100

    ok

--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -6,19 +6,17 @@ export default class Home extends Component {
   render() {
     return (
       <div className="Home-container">
-        <nav className="nav">
-          <ul className="nav__list">
-            <li className="nav__list__item"><Link to="/app">LET ME PLAY</Link></li>
-            <li className="nav__list__item">ABOUT</li>
-          </ul>
-        </nav>
         <div className="bg1"></div>
         <div className="bg2"></div>
         <div className="bg3"></div>
         <div className="bg4"></div>
-        <div className="home">
-          <img className="home__logo" src="/img/mainlogo.gif"></img>
-        </div>
+        <nav className="menu">
+          <img className="menu__logo" src="/img/mainlogo.gif"></img>
+          <ul className="menu__list">
+            <li className="menu__list__item"><Link to="/app" className="play">PLAY</Link></li>
+            <li className="menu__list__item"><Link to="/app">ABOUT</Link></li>
+          </ul>
+        </nav>
       </div>
     );
   }
