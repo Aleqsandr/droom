71e31fdffba1ff37dd9caa1bb4906770c6cc6ca9
Tencap <noisiv@outlook.fr>
2017-01-15 14:25:21 +0100

    edit changes

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -49,12 +49,12 @@ module.exports = {
 
   module: {
     loaders: [
-       {
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          loaders: [
-              'file?hash=sha512&digest=hex&name=[hash].[ext]',
-              'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-          ]
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
       },
       {
         test: /\.scss$/, // sass files
