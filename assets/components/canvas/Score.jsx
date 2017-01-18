7b88b3d4683e3b39acf5b32ee03eb74a2479ace2
Tencap <noisiv@outlook.fr>
2017-01-18 11:03:46 +0100

    multiplier added

--- assets/components/canvas/Score.jsx
+++ assets/components/canvas/Score.jsx
@@ -15,26 +15,58 @@ export default class Score extends Component {
       streak:true,
       prevTiming:null,
       score:null,
+      multiplier:1
     };
   }
 
   componentWillReceiveProps(nextProps) {
     if(prevTime) {
       if(nextProps.timingNote != prevTime){
-        if(nextProps.timingNote>0 && nextProps.timingNote <= 300)
-          this.setState({streak:this.state.streak+1, score:this.state.score+10})
-        else
-          this.setState({streak:0, score:this.state.score-2})
-      } else {
+        if(nextProps.timingNote>0 && nextProps.timingNote <= 300){
+          switch(this.state.streak) {
+            case 3:
+                this.setState({multiplier:2});
+                break;
+            case 5:
+                this.setState({multiplier:3});
+                break;
+            case 7:
+                this.setState({multiplier:4});
+                break;
+            case 10:
+                this.setState({multiplier:5});
+                break;
+          }
 
+          this.setState({streak:this.state.streak+1, score:this.state.score+10*this.state.multiplier});
+        } 
+        else
+          this.setState({streak:0, score:this.state.score-2, multiplier:1})
       }
       prevTime = nextProps.timingNote;
-    } else {
+    } 
+    else {
       prevTime = nextProps.timingNote;
-      if(nextProps.timingNote>0 && nextProps.timingNote <= 300)
-        this.setState({streak:this.state.streak+1, score:this.state.score+10})
+      if(nextProps.timingNote>0 && nextProps.timingNote <= 300){
+        switch(this.state.streak) {
+          case 3:
+              this.setState({multiplier:2});
+              break;
+          case 5:
+              this.setState({multiplier:3});
+              break;
+          case 7:
+              this.setState({multiplier:4});
+              break;
+          case 10:
+              this.setState({multiplier:5});
+              break;
+        }
+
+        this.setState({streak:this.state.streak+1, score:this.state.score+10*this.state.multiplier})
+      }
       else
-        this.setState({streak:0})
+        this.setState({streak:0, multiplier:1})
     }
   }
 
@@ -43,6 +75,7 @@ export default class Score extends Component {
       <div className="score">
         <div className="score__real">Score : {this.state.score}</div>
         <div className="score__streak">Streak : {this.state.streak}</div>
+        <div className="score__multiplier">X {this.state.multiplier}</div>
       </div>
     );
   }
