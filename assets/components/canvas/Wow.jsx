2db4bee52a529a6d48946bdd24b24ba3b36a429a
Aleqsandr <alexandre.reho@gmail.com>
2017-01-20 14:49:16 +0100

    colored and animated WOW is OK

--- assets/components/canvas/Wow.jsx
+++ assets/components/canvas/Wow.jsx
@@ -5,25 +5,49 @@ export default class Wow extends Component {
     super(props);
 
     this.state = {
-      "text":null
+      "text":null,
     };
   }
 
-  componentWillReceiveProps(nextProps) {
+  componentDidUpdate(el,il) {
+    let wow = document.body.querySelector(".wow__text");
+    wow.classList.remove("wow__text--wowin");
+    setTimeout(function(){
+    wow.classList.add("wow__text--wowin");
+    },2)
+
+  }
+
+  componentWillReceiveProps(nextProps)
+  {
     let time = nextProps.timingNote;
-
+    let wow = document.body.querySelector(".wow__text");
     if(time===0) return;
-
     if(time<0)
-      this.setState({text:"really ?!"})
+    {
+      this.setState({text:"really ?! -_-"});
+      wow.style.color = 'red';
+    }
     else if(time<50)
-      this.setState({text:"wow"})
+    {
+      this.setState({text:"wow"});
+      wow.style.color = 'pink';
+    }
     else if(time<100)
-      this.setState({text:"good"})
+    {
+      this.setState({text:"good"});
+      wow.style.color = 'lightgreen';
+    }
     else if(time<200)
-      this.setState({text:"ok"})
+    {
+      this.setState({text:"ok"});
+      wow.style.color = 'blue';
+    }
     else
-      this.setState({text:"bad"})
+    {
+      this.setState({text:"bad"});
+      wow.style.color = 'orange';
+    }
   }
 
   render()
