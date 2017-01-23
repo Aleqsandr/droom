461e484e38aa921294c8cd04bde54368e11fd826
Aleqsandr <alexandre.reho@gmail.com>
2017-01-23 21:25:11 +0100

    track component integrated like a boss mamene

--- assets/components/canvas/Track.jsx
+++ assets/components/canvas/Track.jsx
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 
 export default class Track extends Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            "title": "Killing In The Name Of",
+            "artist": "Rage Against The Machine",
+            "year": 1992,
+            "difficulty": 3
+        };
+    }
+
     render()
     {
         return (
             <div className="trackelement">
-                <div className="live"><p>LIVE</p></div>
-                <div className="repeat"><p>REPEAT</p></div>
+                <div className="trackelement__left">
+                    <p className="tracktitle">{this.state.title}</p>
+                    <p className="trackartist">{this.state.artist}</p>
+                    <div className="trackdetails">
+                        <p className="trackyear">{this.state.year} &mdash; </p>
+                        <p className="trackdifficulty">&#9733;&#9733;&#9733;</p>
+                    </div>
+                </div>
+                <div className="trackelement__right">
+                    <div className="repeat"><p>REPEAT</p></div>
+                    <div className="live"><p>LIVE</p></div>
+                </div>
             </div>
         );
     }
--- assets/components/pages/Menu.jsx
+++ assets/components/pages/Menu.jsx
@@ -41,7 +41,7 @@ export default class Menu extends Component
             	<div className="user">
             		<div className="isconnected"></div>
             		<div className="logout">
-            			<a href="#"><div className="logout__img" ref="logout"></div></a>
+            			<a href="#"><div className="logout__img logout__img--invisible" ref="logout"></div></a>
             		</div>
             		<div className="avatar">
             			<a href="#"><div className="avatar__img" onClick={this.usernamein.bind(this)} ref="triangle"></div></a>
@@ -54,7 +54,15 @@ export default class Menu extends Component
             	<div className="library__slider">
             		<div className="library__slider__tracks">
             			<p className="sectiontitle">TRACKS</p>
-            			<Track/>
+            			<div className="trackwrapper">
+	            			<Track/>
+	            			<Track/>
+	            			<Track/>
+	            			<Track/>
+	            			<Track/>
+	            			<Track/>
+	            			<Track/>
+	            		</div>
             		</div>
             	</div>
             </div>
