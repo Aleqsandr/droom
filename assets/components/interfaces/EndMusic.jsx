5ba89bfe4554666c7b8516431bfd35e63b383bf0
Aleqsandr <alexandre.reho@gmail.com>
2017-01-25 14:43:14 +0100

    end music text updated OK

--- assets/components/interfaces/EndMusic.jsx
+++ assets/components/interfaces/EndMusic.jsx
@@ -3,20 +3,35 @@ import { Link } from 'react-router';
 
 export default class EndMusic extends Component
 {
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
-        console.log(this.props.score.score);
+        if (this.props.score.vanessabar <= 10)
+        {
+            this.state = {
+                "text": "TRY AGAIN"
+            }
+        }
+        else if (this.props.score.vanessabar > 10 && this.props.score.vanessabar <= 200)
+        {
+            this.state = {
+                "text": "BRING IT ON!"
+            }
+        }
+        else if (this.props.score.vanessabar > 200)
+        {
+            this.state = {
+                "text": "YOU ROCK MATE!"
+            }
+        }
     }
 
   render() {
     return (
         <div className="end">
             <div className="end__content">
-                <div className="end__title">End</div>
+                <div className="end__title">{this.state.text}</div>
                 <div className="end__score"> Score : {this.props.score.score}</div>
-                <div className="end__score"> Streak : {this.props.score.streak}</div>
-                <div className="end__score"> VBar : {this.props.score.vanessabar}</div>
+                <div className="end__score"> Streak notes : {this.props.score.streak}</div>
                 <Link to="/menu"><div className="button"><p>MENU</p></div></Link>
             </div>
         </div>
